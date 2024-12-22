989e56327906919430d0a4fc7270fca7d2714826
Amr Elsayed <amr-elsayed@outlook.com>
2024-12-22 21:30:06 +0200

    add more test cases

--- tests/Unit/Models/ArticleTest.php
+++ tests/Unit/Models/ArticleTest.php
@@ -3,6 +3,9 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Article;
+use App\Models\Author;
+use App\Models\Category;
+use App\Models\Source;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -25,5 +28,26 @@ class ArticleTest extends TestCase
             'created_at',
             'id',
         ], array_keys($article->toArray()));
+    }
+
+    public function test_model_has_category_relation()
+    {
+        $artcile = Article::factory()->create();
+
+        $this->assertInstanceOf(Category::class, $artcile->category);
+    }
+
+    public function test_model_has_author_relation()
+    {
+        $artcile = Article::factory()->create();
+
+        $this->assertInstanceOf(Author::class, $artcile->author);
+    }
+
+    public function test_model_has_source_relation()
+    {
+        $artcile = Article::factory()->create();
+
+        $this->assertInstanceOf(Source::class, $artcile->source);
     }
 }
