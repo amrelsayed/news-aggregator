8bb05cdee5e1acee5b23e57600837a61583b0949
Amr Elsayed <amr-elsayed@outlook.com>
2024-12-24 11:37:33 +0200

    caching articles list api

--- app/Actions/Article/ListArticlesAction.php
+++ app/Actions/Article/ListArticlesAction.php
@@ -4,6 +4,7 @@ namespace App\Actions\Article;
 
 use App\Models\Article;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Cache;
 
 class ListArticlesAction
 {
@@ -15,16 +16,33 @@ class ListArticlesAction
      */
     public function excute(array $filters): LengthAwarePaginator
     {
-        $query = Article::query()
-            ->when($filters['keyword'] ?? null, fn ($query, $keyword) => $query->where('title', 'like', "%$keyword%"))
-            ->when($filters['from_date'] ?? null, fn ($query, $date) => $query->whereDate('published_at', '>=', $date))
-            ->when($filters['to_date'] ?? null, fn ($query, $date) => $query->whereDate('published_at', '<=', $date))
-            ->when($filters['category_id'] ?? null, fn ($query, $categoryId) => $query->where('category_id', $categoryId))
-            ->when($filters['author_id'] ?? null, fn ($query, $authorId) => $query->where('author_id', $authorId))
-            ->when($filters['source_id'] ?? null, fn ($query, $sourceId) => $query->where('source_id', $sourceId));
+        $cacheKey = $this->getCacheKey($filters);
 
-        $query->with(['author', 'category', 'source']);
+        $products = Cache::remember($cacheKey, 60 * 60, function () use ($filters) {
+            $query = Article::query()
+                ->when($filters['keyword'] ?? null, fn ($query, $keyword) => $query->where('title', 'like', "%$keyword%"))
+                ->when($filters['from_date'] ?? null, fn ($query, $date) => $query->whereDate('published_at', '>=', $date))
+                ->when($filters['to_date'] ?? null, fn ($query, $date) => $query->whereDate('published_at', '<=', $date))
+                ->when($filters['category_id'] ?? null, fn ($query, $categoryId) => $query->where('category_id', $categoryId))
+                ->when($filters['author_id'] ?? null, fn ($query, $authorId) => $query->where('author_id', $authorId))
+                ->when($filters['source_id'] ?? null, fn ($query, $sourceId) => $query->where('source_id', $sourceId));
 
-        return $query->paginate(10);
+            $query->with(['author', 'category', 'source']);
+
+            return $query->paginate(10);
+        });
+
+        return $products;
+    }
+
+    private function getCacheKey(array $filters): string
+    {
+        $key = 'articles:';
+
+        ksort($filters);
+
+        $query_str = http_build_query($filters);
+
+        return $key.$query_str;
     }
 }
